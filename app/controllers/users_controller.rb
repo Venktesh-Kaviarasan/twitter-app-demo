fdfafe8c4dcda31d05d1636184d1e330f465e900
Venktesh Kaviarasan <venktesh.k@credavenue.com>
2022-01-13 22:15:53 +0530

    added index pages and filters in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
   # before_action :authorized, only: [:show]
+  before_action :logged_in_user, only: [:edit, :update, :show]
+  before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @user = User.all
+  end
 
   def new
     @user = User.new
@@ -45,5 +51,18 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find_by_id(params[:id])
+    flash[:danger] = "Unauthorized" unless current_user?(@user)
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
