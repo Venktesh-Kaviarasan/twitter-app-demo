5a07e5f9ddabbcc22688b1d3c3d2397a47adbcd3
Venktesh Kaviarasan <venktesh.k@credavenue.com>
2022-01-15 11:27:30 +0530

    configured gmail smtp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,8 +42,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => 'venkteshkavi@gmail.com',
-    :password             => 'lgmmjckoqycqwuwq',
+    # :user_name            => 'venkteshkavi@gmail.com',
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
